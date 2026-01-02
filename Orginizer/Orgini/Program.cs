63f9d3a45c67104742ec73b3dcd9159a39e444e8
Zoloz <104018423+ZolozGH@users.noreply.github.com>
2026-01-02 19:43:07 -0400

    Update Program.cs

--- Orginizer/Orgini/Program.cs
+++ Orginizer/Orgini/Program.cs
@@ -1,6 +1,7 @@
-﻿using Orginigger;
+﻿using Orgini;
 
 Console.WriteLine("Orgnizing Main Folders...");
 Console.Write("Paste The Path Of a Directory To Orginize: ");
 string path = Console.ReadLine();
 DirectManager.OrganizeFolder(path);
+
